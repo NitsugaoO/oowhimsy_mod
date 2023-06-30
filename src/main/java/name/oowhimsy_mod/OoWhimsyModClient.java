d5f47115474db95d9fc88f338f3cc2dca91b62dd
NitsugaoO <Nitsuga@qq.com>
2023-06-30 11:12:32 +0800

    Initial commit

---
+++ src/main/java/name/oowhimsy_mod/OoWhimsyModClient.java
@@ -0,0 +1,10 @@
+package name.oowhimsy_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OoWhimsyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/name/oowhimsy_mod/registry/ModItemGroups.java
@@ -0,0 +1,18 @@
+package name.oowhimsy_mod.registry;
+
+import name.oowhimsy_mod.OoWhimsyMod;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups  {
+    public static ItemGroup OOWHIMSY = FabricItemGroup.builder(new Identifier(OoWhimsyMod.MOD_ID, "oowhimsy"))
+            .displayName(Text.translatable("itemgroup.oowhimsy-mod.oowhimsy"))
+            .icon(()-> new ItemStack(ModItems.BAGASSE))
+            .build();
+    public static void registerItemGroup() {
+        OoWhimsyMod.LOGGER.debug("Registering mod item group for " + OoWhimsyMod.MOD_ID);
+    }
+}
